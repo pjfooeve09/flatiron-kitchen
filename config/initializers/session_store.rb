57b2c4aee7d6656415d42a865f08359733b060d3
Parham Javadi <pjfooeve09@yahoo.com>
2021-05-03 13:52:11 -0700

    commit tree 90500e90ca773a3b9f2e02f75e3b77865da12d28

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_flatiron_kitchen_session'
+FlatironKitchen::Application.config.session_store :cookie_store, key: '_flatiron-kitchen_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+FlatironKitchen::Application.routes.draw do
+  resources :recipes
+  resources :ingredients
+  
 end
